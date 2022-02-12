20469230db11ba4ed4fb20bfb308efeaa29e84be
michael <kurashevmichael@gmail.com>
2022-02-12 17:07:59 +0300

    change file views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import json
-
+from django.conf import settings
 
 # Create your views here.
 
@@ -13,6 +13,6 @@ def index(request):
 
 
 def products(request):
-    with open('/home/michael/project_django/geekshop-server/geekshop/products/fixtures/products.json', 'r') as file:
+    with open(settings.BASE_DIR / 'products/fixtures/products.json', 'r') as file:
         context = json.load(file)
     return render(request, "products/products.html", context)
