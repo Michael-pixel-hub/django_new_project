b787c966acf28bf4854ae0863540ade1717f9ec4
michael <kurashevmichael@gmail.com>
2022-02-12 19:59:38 +0300

    add migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    description = models.TextField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
