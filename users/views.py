b71cd66d9bd7532e789101e43a98b13e1c85d78d
michael <kurashevmichael@gmail.com>
2022-02-21 15:18:53 +0300

    added users and added form

---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from users.forms import UserLoginForm
+
+def login(request):
+    form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'GeekShop - Регистрация',
+    }
+    return render(request, 'users/register.html', context)
